c1af2c4ff92b61477a317fe142a41ab7ed87dfde
Jay Grieves <jaygrieves@gmail.com>
2019-08-22 12:59:03 -0500

    Sleep to prevent Docker/MySQL race condition

--- MySqlSupplyCollector/run-tests.sh
+++ MySqlSupplyCollector/run-tests.sh
@@ -2,6 +2,7 @@
 docker run --name mysql-database -p 3300:3306 -e MYSQL_ROOT_PASSWORD=mysqlcontainer123 -e MYSQL_DATABASE=mysql -d mysql:latest
 sleep 20
 docker exec -i mysql-database mysql -uroot -pmysqlcontainer123 mysql < MySqlSupplyCollectorTests/tests/data.sql
+sleep 20
 dotnet test
 docker stop mysql-database
 docker rm mysql-database

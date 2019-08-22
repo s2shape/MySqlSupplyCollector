0514709c6cf3a9d9cf9fa073498e71296c37b6f0
APTTUS\cnidadavole <cnidadavole@apttus.com>
2019-08-22 21:03:11 +0530

    Changes for updating the time limit to 20

--- MySQLsupplyconnector/run-tests.sh
+++ MySQLsupplyconnector/run-tests.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 docker run --name mysql-database -p 3300:3306 -e MYSQL_ROOT_PASSWORD=mysqlcontainer123 -e MYSQL_DATABASE=mysql -d mysql:latest
-sleep 10
+sleep 20
 docker exec -i mysql-database mysql -uroot -pmysqlcontainer123 mysql < MySqlSupplyCollectorTests/tests/data.sql
 dotnet test
 docker stop mysql-database
